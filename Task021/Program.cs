6e7166e941c9809d9ae9c68143303a1ba9881445
Antonio <don_ne@mail.ru>
2022-04-23 12:19:58 +0400

    Task21.1

--- Task021/Program.cs
+++ Task021/Program.cs
@@ -24,4 +24,4 @@ Console.Write("Введите координаты z2: ");
 Double z2 = Convert.ToDouble(Console.ReadLine());
 
 Double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+Console.WriteLine($"A({x1},{y1},{z1});B({x2},{y2},{z2})=> {distance}");
